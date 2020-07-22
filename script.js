64cdef08477eb2a578cfab7781043549b9ba996a
Shubham Pareek <55568207+pareek-shubham@users.noreply.github.com>
2020-07-22 11:02:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,19 +12,10 @@ let openDoor1;
 let openDoor2;
 let openDoor3;
 const isBot = (door) => {
-    if (door.src === botDoorPath) {
-        return true
-    }
-    else {
-        return false;
-    }
+    return (door.src === botDoorPath);
 }
 const isClicked = (door) => {
-    if (door.src === closedDoorPath) {
-        return false;
-    } else {
-        return true;
-    }
+   return (door.src !== closedDoorPath);
 }
 const playDoor = (door) => {
     numClosedDoors--;
@@ -34,38 +25,43 @@ const playDoor = (door) => {
     else if (isBot(door)) {
         gameOver('lose');
     }
-}
+} 
 const randomChoreDoorGenerator = () => {
     const choreDoor = Math.floor(Math.random() * numClosedDoors);
-    if (choreDoor == 0) {
+    switch(choreDoor)
+    {
+    case 0:
         openDoor1 = botDoorPath;
         openDoor2 = beachDoorPath;
         openDoor3 = spaceDoorPath;
-    }
-    else if (choreDoor == 1) {
+        break;
+    case 1:
         openDoor2 = botDoorPath;
         openDoor1 = beachDoorPath;
         openDoor3 = spaceDoorPath;
-    }
-    else {
+        break;
+    case 2:
         openDoor3 = botDoorPath;
         openDoor1 = beachDoorPath;
         openDoor2 = spaceDoorPath;
+        break;
+    default:
+        alert("Something Went Wrong");
     }
 }
-doorImage1.onclick = () => {
+const onImageOneClicked = () => {
     if (currentlyPlaying && !isClicked(doorImage1)) {
         doorImage1.src = openDoor1;
         playDoor(doorImage1);
     }
 }
-doorImage2.onclick = () => {
+const onImageTwoClicked = () => {
     if (currentlyPlaying && !isClicked(doorImage2)) {
         doorImage2.src = openDoor2;
         playDoor(doorImage2);
     }
 }
-doorImage3.onclick = () => {
+const onImageThreeClicked = () => {
     if (currentlyPlaying && !isClicked(doorImage3)) {
         doorImage3.src = openDoor3;
         playDoor(doorImage3);
@@ -81,18 +77,12 @@ const startRound = () => {
     randomChoreDoorGenerator();
 }
 startButton.onclick = () => {
-    if (!currentlyPlaying) {
-        startRound();
-    }
+    (!currentlyPlaying)
+    ? startRound()
+    : null;
 }
 const gameOver = (status) => {
-    if (status === 'win') {
-        startButton.innerHTML = 'You win! Play again?';
-    }
-    else {
-        startButton.innerHTML = 'Game over ! Play again?';
-    }
+    startButton.innerHTML = status === 'win'? 'You win! Play again?':'Game over ! Play again?';
     currentlyPlaying = false;
 }
 startRound();
-
